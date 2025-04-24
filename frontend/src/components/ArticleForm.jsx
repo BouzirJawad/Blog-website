4fe937cd7f8c30d067f3802df51286ba4634a5e7
BouzirJawad <jawadbouzir01@gmail.com>
2025-04-25 00:49:39 +0100

    Update ArticleForm.jsx

--- frontend/src/components/ArticleForm.jsx
+++ frontend/src/components/ArticleForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react'
+import React from 'react'
 import { useFormik } from 'formik'
 import { Article } from '../schemas/Article'
 import axios from 'axios'
@@ -27,9 +27,45 @@ function ArticleForm() {
     })
 
   return (
-    <div>
-     <Toaster position='top-center' />
-    </div>
+    <form onSubmit={formik.handleSubmit} className='flex flex-col gap-4 max-w-md mx-auto' >
+        <Toaster position='top-center' />
+        <div>
+            <label>Title</label>
+            <input type="text" name='title' onChange={formik.handleChange} value={formik.values.title} />
+            {formik.touched.title && formik.errors.title && (
+                <p className='text-red-500 text-sm'>{formik.errors.title}</p>
+            )}
+        </div>
+
+        <div>
+            <label>Image URL</label>
+            <input type="text" name='image' onChange={formik.handleChange} value={formik.values.image} />
+            {formik.touched.image && formik.errors.image && (
+                <p className='text-red-500 text-sm'>{formik.errors.image}</p>
+            )}
+        </div>
+
+        <div>
+            <label>Content</label>
+            <input type="text" name='content' onChange={formik.handleChange} value={formik.values.content} />
+            {formik.touched.content && formik.errors.content && (
+                <p className='text-red-500 text-sm'>{formik.errors.content}</p>
+            )}
+        </div>
+
+        <div>
+            <label>Category</label>
+            <input type="text" name='category' onChange={formik.handleChange} value={formik.values.category} />
+            {formik.touched.category && formik.errors.category && (
+                <p className='text-red-500 text-sm'>{formik.errors.category}</p>
+            )}
+        </div>
+
+        <button type='submit' className='primary-btn' >
+            Create Article
+        </button>
+
+    </form>
   )
 }
 
