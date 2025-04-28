a4966d59a5b51051526f80e66fc7637e2dad7855
LAKHMAISS CHAIMA <chaimaalakhmaiss23@gmail.com>
2025-04-28 12:05:03 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Connect from "./pages/Connect";
-import MainPage from "./pages/MainPage";
+import React from 'react';
+import Connect from './pages/Connect';
+import Header from './components/Header';
+import ArticleForm from './components/ArticleForm';
+import { Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="*" element={<MainPage /> }/>
-          <Route path="/Connect" element={<Connect />} />
-        </Routes>
-      </Router>
+    <div className="h-screen w-screen bg-gradient-to-br from-slate-800 to-slate-900 flex flex-col">
+      <Header />
+      
+      <div className="flex-grow flex justify-center items-center">
+        <div className="backdrop-blur-md bg-white/10 border border-white/20 rounded-xl p-10 shadow-xl w-full max-w-4xl mx-auto">
+          <Routes>
+            <Route path="/*" element={<Connect />} />
+            <Route path="/new-article" element={<ArticleForm />} />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
